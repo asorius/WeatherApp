b5bbd1e7e4f3e6d23e4e9593608a8f82f99f8b0f
ddexta <18assdd@gmail.com>
2018-11-22 18:22:48 +0000

    deploying stuff5

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,18 +5,3 @@ export default class Footer extends Component {
     return <div className="page-footer">made by me c</div>;
   }
 }
-//--------------Google places for drop down return CORB error....
-// const key = 'AIzaSyBJpOSfZ7ox4FZau_RaPCXtx3kJPy4Mmkc';
-// try {
-//   const response = await axios({
-//     method: 'get',
-//     url: `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${text}&&types=(cities)&key=${key}`,
-//     headers: {
-//       'Access-Control-Allow-Origin': '*'
-//     }
-//   });
-//   const list = response.data.map(res => res.description);
-//   console.log(list);
-// } catch (e) {
-//   console.log(e);
-// }
