ded7d53c81c12270191c28239b3505858227130f
ddexta <18assdd@gmail.com>
2018-11-22 17:59:35 +0000

    Updates

---
+++ precache-manifest.263e3c64f026befc944364be8329caf1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdcbed4f035fa28e335f2ac2aaa363ac",
+    "url": "/weather/static/media/gratisography-greyscale-city-landscape.fdcbed4f.jpg"
+  },
+  {
+    "revision": "177e1b728fb0d4ffdfea",
+    "url": "/weather/static/js/runtime~main.177e1b72.js"
+  },
+  {
+    "revision": "dc14175b3d1aff234a8a",
+    "url": "/weather/static/js/main.dc14175b.chunk.js"
+  },
+  {
+    "revision": "c21515558a7a7ccce0f9",
+    "url": "/weather/static/js/1.c2151555.chunk.js"
+  },
+  {
+    "revision": "dc14175b3d1aff234a8a",
+    "url": "/weather/static/css/main.68015aa2.chunk.css"
+  },
+  {
+    "revision": "67a81372f4d7ae4575ef099846c8d0ce",
+    "url": "/weather/index.html"
+  }
+];
